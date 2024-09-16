51ecf4ee565284d8f4ae8b116cc75566b826c846
fbohling <fbohling@student.42wolfsburg.de>
2024-09-16 17:37:20 +0200

    test on vm

---
+++ Makefile
@@ -0,0 +1,22 @@
+build:
+	docker-compose -f srcs/docker-compose.yml up --build
+
+up:
+	docker-compose -f srcs/docker-compose.yml up -d 
+
+down:
+	docker-compose -f srcs/docker-compose.yml down
+
+clear_volumes:
+	docker-compose -f srcs/docker-compose.yml down -v
+
+clean: down
+	docker system prune -a --volumes -f
+
+dry-run:
+	docker-compose -f srcs/docker-compose.yml --dry-run up --build -d
+
+re:
+	docker-compose -f srcs/docker-compose.yml restart
+	
+.PHONY: build up down clean dry-run re
---
+++ srcs/requirements/wordpress/tools/script.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+if [ -f ./wp-config.php ]
+then
+	echo "wordpress already installed"
+else
+	wp core download --path=/var/www/html --allow-root
+	wp config create --dbname=$WORDPRESS_DB_NAME --dbuser=$WORDPRESS_DB_USER --dbpass=$(cat $WORDPRESS_DB_PASSWORD_FILE) --dbhost=$WORDPRESS_DB_HOST --allow-root
+	wp core install --url=$WORDPRESS_URL --title="Freddy's Inception" --admin_user=$WORDPRESS_ADMIN_USER --admin_password=$(cat $WORDPRESS_ADMIN_PASSWORD_FILE) --admin_email=$WORDPRESS_ADMIN_EMAIL --allow-root
+	wp user create bobby bob@mcbob.com --role=author --user_pass=$WP_USER_PASS --porcelain --allow-root
+	wp theme install boardwalk --activate --allow-root
+	wp theme install cubic --activate --allow-root
+	echo "wordpress installed"
+fi
+
+exec "$@"
